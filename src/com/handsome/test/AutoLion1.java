443d02dc158dba3d1b54bbd457c4a1f16d620e56
kelvinyipsir <kelvinyipman@outlook.com>
2017-12-11 12:04:29 +0800

    live

---
+++ src/com/handsome/test/AutoLion1.java
@@ -0,0 +1,17 @@
+package com.handsome.test;
+
+public class AutoLion1 {
+	String color ="黄色";
+	public void run() {
+		System.out.println("正以0.1米/秒的速度向前奔跑");
+	}
+	public void cry(){
+		System.out.println("大声吠");
+	}
+	public String robBall() {
+		String ball = "球";
+		return ball;
+		
+	}
+
+}
